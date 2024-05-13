5267434072e3cdaf817137b7b182c94576d7f0a8
codercgh <codercgh@gmail.com>
2024-05-14 03:57:43 +0800

    update tailwindcss

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,4 +1,4 @@
-<template><h1>Hello World!</h1></template>
+<template><h1 class="text-3xl font-bold underline">Hello World!</h1></template>
 
 <script setup lang="ts"></script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import '@/assets/base/tailwind.css'
 import App from './App.vue'
 import router from './router'
 import { setupStore } from './store'
