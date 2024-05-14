1a43a9741491f8def76a5b8b4ba7f1974ad3c412
codercgh <codercgh@gmail.com>
2024-05-15 01:33:58 +0800

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,4 +1,11 @@
-<template><h1 class="text-3xl font-bold underline">Hello World!</h1></template>
+<template>
+  <h1
+    class="text-3xl font-bold underline bg-gradient-to-r from-cyan-500 to-blue-500"
+  >
+    Hello World!
+  </h1>
+  <div class="btn-blue">332323</div>
+</template>
 
 <script setup lang="ts"></script>
 
